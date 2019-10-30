01f9e2a713a59b5c87ab3571cd57d05b69417c1f
Andrew Rowson <github@growse.com>
2019-10-30 23:01:36 +0000

    Travis: corrected zipalign and jarsigner path and key alias

--- project/build_on_travis.sh
+++ project/build_on_travis.sh
@@ -6,7 +6,7 @@ set -ev
 
 if [ "${TRAVIS_TAG}" != "" ]; then
   echo "Tagged with: $TRAVIS_TAG"
-  jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks app/build/outputs/apk/app-release-unsigned.apk fastlanetraviscipoc
-  /usr/local/android-sdk/build-tools/23.0.3/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
+  jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore ../android.jks ./app/build/outputs/apk/release/app-release-unsigned.apk owntracks
+  /usr/local/android-sdk/build-tools/29.0.2/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
   ls -la app/build/outputs/apk
 fi

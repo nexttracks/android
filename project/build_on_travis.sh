89edf497f0e62f4c14ec150c3e5f520319dfefc7
Andrew Rowson <github@growse.com>
2019-11-01 10:52:17 +0000

    Update build_on_travis.sh
    Fixed paths for jarsigner

--- project/build_on_travis.sh
+++ project/build_on_travis.sh
@@ -7,6 +7,6 @@ set -ev
 if [ "${TRAVIS_TAG}" != "" ]; then
   echo "Tagged with: $TRAVIS_TAG"
   jarsigner -verbose -sigalg SHA1withRSA -storepass $keystorepass -keypass $keystorepass -digestalg SHA1 -keystore ../keystore.jks ./app/build/outputs/apk/release/app-release-unsigned.apk owntracks
-  /usr/local/android-sdk/build-tools/29.0.2/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
-  ls -la app/build/outputs/apk
+  /usr/local/android-sdk/build-tools/29.0.2/zipalign -v 4 ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/release/app-release.apk
+  find app/build/outputs/apk
 fi

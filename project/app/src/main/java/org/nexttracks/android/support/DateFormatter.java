e4d481136879a6ea39b454108d55856f453709f1
nycex <nycex@cccp.org>
2020-06-15 17:59:30 +0200

    clean up dateformatter

--- project/app/src/main/java/org/nexttracks/android/support/DateFormatter.java
+++ project/app/src/main/java/org/nexttracks/android/support/DateFormatter.java
@@ -10,29 +10,22 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class DateFormatter {
-    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm",Locale.getDefault());
-    private static SimpleDateFormat dateFormatterToday = new SimpleDateFormat("HH:mm", Locale.getDefault());
-    private static SimpleDateFormat dateFormatterDate = new SimpleDateFormat("HH:mm", Locale.getDefault());
+    private static SimpleDateFormat dateFormatterFull = new SimpleDateFormat("yyyy-MM-dd HH:mm",Locale.getDefault());
+    private static SimpleDateFormat dateFormatterTime = new SimpleDateFormat("HH:mm", Locale.getDefault());
 
+    public static String formatDateShort(long tstSeconds) {
+        return dateFormatterTime.format(new Date(TimeUnit.SECONDS.toMillis(tstSeconds)));
+    }
 
     public static String formatDate(long tstSeconds) {
         return formatDate(new Date(TimeUnit.SECONDS.toMillis(tstSeconds)));
     }
 
-    public static String formatDateShort(long tstSeconds) {
-        Date d = new Date(TimeUnit.SECONDS.toMillis(tstSeconds));
-        if(DateUtils.isToday(d.getTime())) {
-            return dateFormatterToday.format(d);
-        } else {
-            return dateFormatterDate.format(d);
-        }
-    }
-
     public static String formatDate(@NonNull Date d) {
         if(DateUtils.isToday(d.getTime())) {
-            return dateFormatterToday.format(d);
+            return dateFormatterTime.format(d);
         } else {
-            return dateFormatter.format(d);
+            return dateFormatterFull.format(d);
         }
     }
 }
